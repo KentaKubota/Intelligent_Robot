d1a4a26e7a824e507530911cdb3711ce6210252c
Kenta Kubota <zingl.0523@gmail.com>
2016-01-06 22:30:29 +0900

    finished

--- LineTrace.cpp
+++ LineTrace.cpp
@@ -8,6 +8,12 @@ DigitalIn ss1(p5),ss2(p6),ss3(p7),ss4(p8);
 
 float target_speed_r, target_speed_l; 
 
+
+/********************************************************************/
+/*                     ライントレース関数                           */
+/*          引数: 止まりたいクロスラインまでのクロスラインの本数    */
+/*                    信号はデジタル出力                            */
+/********************************************************************/
 void LineTrace(int countxline)
 {
     int sw = 0;
@@ -20,11 +26,10 @@ void LineTrace(int countxline)
        s3 = ss3.read();
        s4 = ss4.read();
 
-       if(s1 == 1 && s2 == 0 && s3 == 1 && s4 == 1) sw = 1;
+       if(s1 == 1 && s2 == 0 && s3 == 1 && s4 == 1)      sw = 1;
        else if(s1 == 1 && s2 == 1 && s3 == 0 && s4 == 1) sw = 2;
        else if(s1 == 0 && s2 == 1 && s3 == 1 && s4 == 1) sw = 3;
        else if(s1 == 1 && s2 == 1 && s3 == 1 && s4 == 0) sw = 4;
-
        else if(s1 == 0 && s2 == 0 && s3 == 1 && s4 == 1) sw = 5;
        else if(s1 == 1 && s2 == 1 && s3 == 0 && s4 == 0) sw = 6;
        else if(s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0) sw = 7;
@@ -32,67 +37,56 @@ void LineTrace(int countxline)
        else if(s1 == 1 && s2 == 0 && s3 == 0 && s4 == 0) sw = 7;
        else if(s1 == 1 && s2 == 0 && s3 == 0 && s4 == 1) sw = 7;
 
-
-
        switch(sw)
        {
            case 1:
                target_speed_r = 220, target_speed_l = 480; 
                break;
-
            case 2:
                target_speed_r = 480, target_speed_l = 220;
                break;
-
            case 3:
                target_speed_r = 30, target_speed_l = 460;
                break;
-
            case 4:
                target_speed_r = 460, target_speed_l = 30;
                break;
-
            case 5:
                target_speed_r = 60, target_speed_l = 460;
                break;
-
            case 6:
                target_speed_r = 460, target_speed_l = 60;
                break;
-
            case 7:
                target_speed_r = target_speed_l = 380;
-
                count += 1;
                if(count == countxline) target_speed_r = target_speed_l = 240;
-               //printf("count = %d\n",count);
-               wait(0.09);//0.17???
+               wait(0.09);
                break;
 
            default:
                target_speed_r = target_speed_l = 410;
                break;
        }
-
     sw = 0;
     } 
     target_speed_r = target_speed_l = 0;
 }
 
 
-
-
-
+/********************************************************************/
+/*                 オドメトリを用いたライントレース関数             */
+/*                 引数: 進みたい距離[mm]                           */
+/********************************************************************/
 void MoveLineTrace(float distance)
 {
-    x1 = y1 = tht1 = 0;
-    x0 = y0 = tht0 = 0;
-
     int sw = 0;
     int s1,s2,s3,s4;
+
+    x1 = y1 = tht1 = 0;
+    x0 = y0 = tht0 = 0;
     while(x1 <= distance)
     {
-       //printf("x1 = %f\n",x1*10);
        s1 = ss1.read();
        s2 = ss2.read();
        s3 = ss3.read();
@@ -106,26 +100,21 @@ void MoveLineTrace(float distance)
        switch(sw)
        {
            case 1:
-               target_speed_r = 120, target_speed_l = 320;//230
+               target_speed_r = 120, target_speed_l = 320;
                break;
-
            case 2:
-               target_speed_r = 320, target_speed_l = 120;///100 70
+               target_speed_r = 320, target_speed_l = 120;
                break;
-
            case 3:
                target_speed_r = 50, target_speed_l = 350;
                break;
-
            case 4:
                target_speed_r = 350, target_speed_l = 50;
                break;
-
            default:
                target_speed_r = target_speed_l = 300;
                break;
        }
-
     sw = 0;
     } 
     target_speed_r = target_speed_l = 0;
--- PSD.cpp
+++ PSD.cpp
@@ -5,9 +5,14 @@
 #define NUM 9
 AnalogIn PSD(p16);     
 
-
 int comp( const void *c1, const void *c2 );
 
+
+/********************************************************************/
+/*                     PSDセンサ関数                                */
+/*                     引数: なし                                   */
+/*                     戻り値:　アナログ信号                        */
+/********************************************************************/
 float PSD_Value()
 {
     float sort[NUM] = {0};
@@ -15,10 +20,8 @@ float PSD_Value()
     for(int i = 0; i < NUM; i++)
         sort[i] = PSD.read();
     qsort(sort, NUM, sizeof(float), comp);
-    //printf("%f\n",sort[4]);
 
     return sort[(NUM -1) / 2 ];
-
 }
 
 int comp( const void *c1, const void *c2 )
@@ -27,7 +30,6 @@ int comp( const void *c1, const void *c2 )
   int tmp2 = *(int *)c2;
 
   if( tmp1 < tmp2 )  return -1;
-//  if( tmp1 == tmp2 ) return  0;
   if( tmp1 > tmp2 )  return  1;
-  return  0;//////////////////////////////
+  return 0;
 }
